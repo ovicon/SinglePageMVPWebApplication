09ea98b3931b55a563b19d8cb2e80af095a85d0f
Ovidiu CONEAC <ovidiu.coneac@gmail.com>
2017-04-11 17:46:13 +0300

    Implemented web service skeleton

---
+++ common/src/main/java/ro/ovidiuconeac/users/common/Delete.java
@@ -0,0 +1,7 @@
+package ro.ovidiuconeac.users.common;
+
+/**
+ * Created by ovidiu on 4/11/17.
+ */
+public class Delete {
+}
---
+++ web-service/src/main/java/ro/ovidiuconeac/users/webservice/Main.java
@@ -0,0 +1,16 @@
+package ro.ovidiuconeac.users.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Ovidiu CONEAC on 2/12/2017.
+ */
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+   }
+}
