c060edd10fb6ec4a7e9f083bb01058acf8eeaee6
Ovidiu CONEAC <ovidiu.coneac@gmail.com>
2017-05-15 20:20:45 +0300

    No more android development for this example

--- android-application/app/src/main/java/ro/ovidiuconeac/users/androidapp/components/newuser/mvp/presenter/NewUserPresenter.java
+++
@@ -1,9 +0,0 @@
-package ro.ovidiuconeac.users.androidapp.components.newuser.mvp.presenter;
-
-/**
- * Created by www.ovidiuconeac.ro on 4/18/17.
- */
-
-public interface NewUserPresenter {
-    void requestSaveUser();
-}
